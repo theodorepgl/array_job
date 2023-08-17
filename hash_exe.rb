0697b3b920702d9de9b26a65c48ddd6d6208e085
theodorepgl <limlimtheo96@gmail.com>
2023-08-17 12:46:41 +0800

    first commit

---
+++ hash_exe.rb
@@ -0,0 +1,10 @@
+#1. Word Frequency Counter
+#a. Write a method that takes a string and returns a hash where the keys are words in the string and the values are the frequencies of those words.
+#2. Hash Merge
+#a. Write a method that takes two hashes and merges them into a single hash, combining values for common keys.
+#3. Hash Sorting
+#a. Write a method that takes a hash and returns a new hash with the keys sorted alphabetically.
+#4. Hash Inversion
+#a. Write a method that inverts a hash, swapping keys and values.
+#5. Hash Lookup
+#a. Write a method that takes a hash and a key, and returns the value associated with that key. If the key is not found, return a default value.
